58a15492d8e6c56a34a1b8773944d4e956002fcc
Jettucis <aleharijs@gmail.com>
2025-06-24 03:24:37 +0300

    additional storage rift marker in merchant's guild

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 /* Base Image Properties */
 // Cache buster: Needs to be changed every time that the map tiles are updated
-const cache_buster = '50515'
+const cache_buster = '50624'
 // Image dimensions in pixels
 const image_width = 27168
 const image_height = 14400
