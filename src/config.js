8bd5f973e809a3a5c6d1071e877561d3c05f1544
Jettucis <45320856+Jettucis@users.noreply.github.com>
2025-11-16 22:44:21 +0200

    11th of november update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 /* Base Image Properties */
 // Cache buster: Needs to be changed every time that the map tiles are updated
-const cache_buster = '51110'
+const cache_buster = '51111'
 // Image dimensions in pixels
 const image_width = 28464
 const image_height = 15696
