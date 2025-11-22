29de71cf3a7548abf19d7db6590c257972ea602e
Jettucis <45320856+Jettucis@users.noreply.github.com>
2025-11-22 12:43:33 +0200

    17th-19th of november entities
    + remove Mask maker as event has ended

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 /* Base Image Properties */
 // Cache buster: Needs to be changed every time that the map tiles are updated
-const cache_buster = '51117'
+const cache_buster = '51119'
 // Image dimensions in pixels
 const image_width = 28464
 const image_height = 19008
